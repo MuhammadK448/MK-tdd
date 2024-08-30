e2d6fd7c5559987330ef4a3fe6d4d498717e18d3
MJK <muhammadkhusravi@gmail.com>
2024-08-29 21:03:13 -0700

    Activity2 Positive and Negative

--- src/test/java/playground/Activity2.java
+++ src/test/java/playground/Activity2.java
@@ -1,4 +1,6 @@
 package playground;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Activity2 {
@@ -11,14 +13,48 @@ public class Activity2 {
            throw new RuntimeException("First Name or last name can't be null or empty");
        lastName = lastName.toUpperCase();
        firstName = firstName.substring(0, 1).toUpperCase()
-                   + firstName.substring(1);
+                   + firstName.substring(1).toLowerCase();
         return lastName+ ", " + firstName ;
     }
 
-    @Test
-    public void getFullName(){
-        String name = fullNameGenerator("Muhammad", "Khusravi");
+    @Test(dataProvider = "positiveTesting")
+    public void testGettingFullName(String firstName, String lastName, String expectedFullName){
+        String name = fullNameGenerator(firstName, lastName);
         System.out.println(name);
+        Assert.assertEquals(name, expectedFullName, "FullName should match the format");
+    }
+
+    @DataProvider(name = "positiveTesting")
+    public String [][] positiveTesting(){
+        String[][] data = {
+                {"Muhammad", "Abbas", "ABBAS, Muhammad"},
+                {"Saeed", "Khan", "KHAN, Saeed"},
+                {"Mike", "Johnson", "JOHNSON, Mike"}
+        };
+        return data;
+    }
+
+    @Test
+    public void negativeTesting(){
+        try{
+            fullNameGenerator(null, null);
+            Assert.fail("Test supposed to throw Exception");
+        }catch (RuntimeException re){
+            Assert.assertTrue(true, "Catch Exception passing the test");
+        }
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testNegativeWithExpectedException(){
+        fullNameGenerator("", null);
+    }
+
+    //Using lambda
+    @Test
+    public void testNegativeWithAssertTrhow(){
+        Assert.assertThrows(RuntimeException.class, () -> {
+            fullNameGenerator("", "");
+        });
     }
 }
 
