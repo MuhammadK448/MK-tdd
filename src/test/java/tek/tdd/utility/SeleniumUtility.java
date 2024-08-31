13314ae3490ebd8b41926f91978f6fb526c9fbe0
MJK <muhammadkhusravi@gmail.com>
2024-08-31 00:55:58 -0700

    SeleniumUtility class added

---
+++ src/test/java/tek/tdd/utility/SeleniumUtility.java
@@ -0,0 +1,13 @@
+package tek.tdd.utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import tek.tdd.base.BaseSetup;
+
+import java.time.Duration;
+
+public class SeleniumUtility extends BaseSetup {
+    private WebDriverWait getWait() {
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(WAIT_IN_SECONDS));
+    }
+}
