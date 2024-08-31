b0fa6234eb25926c31fd50b7ff8f44737892ceac
MJK <muhammadkhusravi@gmail.com>
2024-08-31 11:57:02 -0700

    UIBase class added

---
+++ src/test/java/tek/tdd/base/UIBaseClass.java
@@ -0,0 +1,23 @@
+package tek.tdd.base;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import tek.tdd.utility.SeleniumUtility;
+
+public class UIBaseClass extends SeleniumUtility {
+    private static final Logger LOGGER = LogManager.getLogger(UIBaseClass.class);
+
+    @BeforeMethod
+    public void setupTest(){
+        LOGGER.info("Setup tes and open Browser");
+        setupBrowser();
+    }
+
+    @AfterMethod
+    public void testCleanup(){
+        LOGGER.info("Closes browser after each test");
+        quitBrowser();
+    }
+}
